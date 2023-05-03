e6c33fb05a234518bf434e6ab4738601e4a4289c
DanielKriegerS <danielschifelbain@gmail.com>
2023-05-03 17:29:16 -0300

    Add endGame and scores

--- scripts/ticTacToe.js
+++ scripts/ticTacToe.js
@@ -1,12 +1,21 @@
 const playerOne = document.querySelector('.playerOne');
 const playerTwo = document.querySelector('.playerTwo');
 const submit = document.querySelector('.submit');
-const grid = document.querySelector('.grid');
 const table = document.querySelector('.table');
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
-var playerTwoInput = document.querySelector('.playerTwoInput');
+const grid = Array.from(Array(3), () => new Array(3).fill(''));
+const gridContainer = document.querySelector('.grid');
 const playerTwoForm = document.querySelector('#playerTwoForm');
+const squares = document.querySelectorAll('.square'); 
+let currentPlayer = 'X';
+let scoreX = 0;
+let scoreO = 0;
+
+localStorage.setItem('scoreX', scoreX);
+localStorage.setItem('scoreO', scoreO);
+
+var playerTwoInput = document.querySelector('.playerTwoInput');
 
 const addPlayer = () => {
     if(playerTwoInput){
@@ -28,14 +37,52 @@ playerTwoForm.addEventListener('submit', (event) => {
   }
 });
 
-const squares = document.querySelectorAll('.square'); 
+function checkEndGame() {
+  for (let i = 0; i < 3; i++) {
+    if (grid[i][0] !== '' && grid[i][0] === grid[i][1] && grid[i][1] === grid[i][2]) {
+      if(grid[i][0] == "X"){
+        scoreX++;
+        localStorage.setItem('scoreX', scoreX);
+      } else{
+        scoreO++;
+        localStorage.setItem('scoreO', scoreO);
+      }
+    }
+  }
 
-let currentPlayer = 'X'; 
+  for (let j = 0; j < 3; j++) {
+    if (grid[0][j] !== '' && grid[0][j] === grid[1][j] && grid[1][j] === grid[2][j]) {
+      if(grid[0][j] == 'X'){
+        scoreX++;
+        localStorage.setItem('scoreX', scoreX);
+      } else{
+        scoreO++;
+        localStorage.setItem('scoreO', scoreO);
+      }
+    }
+  }
+
+  if (grid[0][0] !== '' && grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]) {
+    if(grid[0][0] == 'X'){
+      scoreX++;
+      localStorage.setItem('scoreX', scoreX);
+    } else{
+      scoreO++;
+      localStorage.setItem('scoreO', scoreO);
+    }
+  }
+  if (grid[0][2] !== '' && grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]) {
+    if(grid[0][2] == 'X'){
+      scoreX++;
+      localStorage.setItem('scoreX', scoreX);
+    } else{
+      scoreO++;
+      localStorage.setItem('scoreO', scoreO);
+    }
+  }
+}
+ 
 const createGrid = () => {
-    const grid = Array.from(Array(3), () => new Array(3).fill(''));
-  
-    const gridContainer = document.querySelector('.grid');
-  
     for (let row = 0; row < 3; row++) {
       for (let col = 0; col < 3; col++) {
         const button = document.createElement('button');
@@ -49,6 +96,7 @@ const createGrid = () => {
             button.textContent = currentPlayer;
             grid[row][col] = currentPlayer;
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            checkEndGame();
           }
         });
       }
@@ -65,11 +113,15 @@ squares.forEach(square => {
 });
 
 const loadGame = () => {
-    if (grid.children.length > 0) {
+    if (gridContainer.children.length > 0) {
         while (grid.firstChild) {
       grid.removeChild(grid.firstChild);
     }
  }  
+}
+
+const backToLogin = () => {
+  window.location = '../index.html';
 }
 
 window.onload = () => {
